71112a7920d7324a03ba732634fa77eedf1c8019
n4o847 <22975590+n4o847@users.noreply.github.com>
2022-01-11 15:25:49 +0900

    Fix bash scripts

--- examples/rust/fizzbuzz/run.sh
+++ examples/rust/fizzbuzz/run.sh
@@ -10,7 +10,7 @@ echo $RUSTUP_HOME
 RUSTUP_ACTIVE_TOOLCHAIN=$(rustup show active-toolchain | awk '{ print $1 }')
 echo $RUSTUP_ACTIVE_TOOLCHAIN
 
-LIB_DIR=$RUSTUP_HOME/toolchains/$RUSTUP_ACTIVE_TOOLCHAIN/lib/
+LIB_DIR="$RUSTUP_HOME/toolchains/$RUSTUP_ACTIVE_TOOLCHAIN/lib/"
 echo $LIB_DIR
 
 LIB_STD=$(ls $LIB_DIR | grep ^libstd- | sed -e 's/^lib//' -e 's/\.so$//')
@@ -19,7 +19,7 @@ echo $LIB_STD
 
 # Compilation
 
-echo Compiling...
+echo "Compiling..."
 
 mkdir -p bitcode
 
@@ -27,11 +27,11 @@ rustc -g --emit=llvm-bc src/main.rs -o bitcode/main.bc
 
 $AFLV cc -L $LIB_DIR -l $LIB_STD bitcode/main.bc -o main
 
-echo Compilation finished
+echo "Compilation finished"
 
 
 # Fuzzing
 
-echo Start fuzzing
+echo "Start fuzzing"
 
 LD_LIBRARY_PATH=$LIB_DIR $AFLV fuzz -i in -o out -- ./main
